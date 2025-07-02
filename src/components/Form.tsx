db800875d430c2399618be229cc601158d9906da
gdudek <gdudek.dev@gmail.com>
2025-07-02 13:45:18 +0200

    REACT/REFACTOR/ saving user is clearer now

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,10 +5,11 @@ export default function Form({ users,setUsers }) {
   const [username, setUsername] = useState("");
   function handleSubmit(e) {
     e.preventDefault();
-    setUsers((prev) => [...prev, { user: username, phone: phoneNumber }]);
+    const update =  [...users, { user: username, phone: phoneNumber }];
+    setUsers(update);
     setPhoneNumber("");
     setUsername("");
-    localStorage.setItem("users", JSON.stringify([...users, {user: username, phone: phoneNumber}]))
+    localStorage.setItem("users", JSON.stringify(update))
   }
   return (
     <form onSubmit={handleSubmit} className="flex gap-4">
@@ -20,7 +21,7 @@ export default function Form({ users,setUsers }) {
         required
         id="phone-input"
         aria-describedby="helper-text-explanation"
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-4 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         
       />
       <input
@@ -28,7 +29,7 @@ export default function Form({ users,setUsers }) {
         onChange={(e) => setUsername(e.target.value)}
         value={username}
         placeholder="Name"
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-4 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         required
       />
       <input
