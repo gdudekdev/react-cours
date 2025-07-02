87f1b4ba1a840831001841252874c4df6861817f
gdudek <gdudek.dev@gmail.com>
2025-07-02 09:54:49 +0200

    REACT/FEAT/ adding users and phone numbers to form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,15 @@
-import { use, useState } from "react";
+import { useState } from "react";
 
-export default function Form({ setPhoneNumbers }) {
+export default function Form({ setUsers, setPhoneNumbers, setNbUsers }) {
   const [phoneNumber, setPhoneNumber] = useState("");
-
+  const [username, setUsername] = useState("");
   function handleSubmit(e) {
     e.preventDefault();
     setPhoneNumbers((prev) => [...prev, phoneNumber]);
+    setUsers((prev) => [...prev, username]);
+    setNbUsers((prev) => prev + 1);
     setPhoneNumber("");
+    setUsername("");
   }
   return (
     <form onSubmit={handleSubmit} className="flex gap-4">
@@ -15,6 +18,14 @@ export default function Form({ setPhoneNumbers }) {
         onChange={(e) => setPhoneNumber(e.target.value)}
         value={phoneNumber}
         placeholder="Enter your phone number"
+        className="p-2 rounded-sm"
+      />
+      <input
+        type="text"
+        onChange={(e) => setUsername(e.target.value)}
+        value={username}
+        placeholder="Enter your name"
+        className="p-2 rounded-sm"
       />
       <input type="submit" value="Confirm" />
     </form>
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -3,20 +3,19 @@ import User from "./User";
 import Form from "./Form";
 
 export default function UserList() {
-  const [users, setUsers] = useState([
-    "John Doe",
-    "Jane Smith",
-    "Alice Johnsson",
-  ]);
+  const [users, setUsers] = useState([]);
   const [phoneNumbers, setPhoneNumbers] = useState([]);
   const [nbUsers, setNbUsers] = useState(users.length);
-
 
   return (
     <div className="flex flex-col gap-4 w-full justify-center">
       <div className="flex flex-col gap-4 fixed top-8 left-1/2 -translate-x-1/2">
         <h1 className="">{nbUsers} utilisateurs</h1>
-        <Form setPhoneNumbers={setPhoneNumbers} />
+        <Form
+          setPhoneNumbers={setPhoneNumbers}
+          setUsers={setUsers}
+          setNbUsers={setNbUsers}
+        />
       </div>
       <ul className="flex flex-col gap-4 justify-center items-center h-[40vh] w-full overflow-auto pt-10 p-8">
         {users.map((item, index) => (
